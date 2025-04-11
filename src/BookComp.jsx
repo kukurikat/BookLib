a4692f90a26e87b4835792a3d7165a3c30bde0a0
kukurikat <maks2701207@gmail.com>
2025-04-11 20:05:06 +0300

    changed console log

--- src/BookComp.jsx
+++ src/BookComp.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 function BookComp({ search }) {
   const resp = 0;
-  console.log(search.text);
+  console.log(search);
   return <p>{search.text}</p>;
 }
 export default BookComp;
